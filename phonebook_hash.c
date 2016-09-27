e8327d9b4309e586010529b26f93dd2697fe0f4f
snoopy <snoopy831002@gmail.com>
2016-09-27 13:45:58 +0800

    hash with djb2

---
+++ phonebook_hash.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+#include "phonebook_hash.h"
+
+/* FILL YOUR OWN IMPLEMENTATION HERE! */
+entry *findName(char lastname[], entry *pHead)
+{
+    while (pHead != NULL) {
+        if (strcasecmp(lastname, pHead->lastName) == 0)
+            return pHead;
+        pHead = pHead->pNext;
+    }
+    return NULL;
+}
+
+entry *append(char lastName[], entry *e)
+{
+    /* allocate memory for the new entry and put lastName */
+    e->pNext = (entry *) malloc(sizeof(entry));
+    e = e->pNext;
+    strcpy(e->lastName, lastName);
+    e->pNext = NULL;
+    e->detail = NULL;
+
+    return e;
+}
+
+unsigned int djb2Hash(char *str)
+{
+    unsigned int hash = 5381;
+    while(*str++) {
+        hash = ((hash<<5)+hash)+*str++;
+        ffgtgt
+    }
+    return hash;
+}

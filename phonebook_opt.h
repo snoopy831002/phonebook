967e6512fb8d81872b9122d06947374db2ef5fbf
snoopy <snoopy831002@gmail.com>
2016-09-27 16:51:48 +0800

    update CFLAG in MakeFile

--- phonebook_opt.h
+++ phonebook_opt.h
@@ -4,9 +4,6 @@
 
 #define MAX_LAST_NAME_SIZE 16
 
-/* TODO: After modifying the original version, uncomment the following
- * line to set OPT properly */
-// #define OPT 1
 typedef struct __PHONE_BOOK_ENTRY {
     char lastName[MAX_LAST_NAME_SIZE];
     char firstName[16];
@@ -23,7 +20,8 @@ typedef struct __PHONE_BOOK_ENTRY {
 
 typedef struct __LAST_NAME_ENTRY {
     char lastName[MAX_LAST_NAME_SIZE];
-    struct __PHONE_BOOK_ENTRY *pNext;
+    struct entry_detail  *detail;
+    struct __LAST_NAME_ENTRY*pNext;
 } entry;
 
 entry *findName(char lastname[], entry *pHead);
